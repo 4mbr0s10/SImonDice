70c2c45e1661fcb0b696a1443126345b0c793d8e
Héctor Pérez Ambrosio <hperezambrosio@danielcastelao.org>
2022-10-18 00:32:39 +0200

    Commit 3

--- app/src/main/java/com/example/simondice/Botones.kt
+++ app/src/main/java/com/example/simondice/Botones.kt
@@ -2,25 +2,62 @@ package com.example.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Botones : AppCompatActivity() {
+    var listaColoresMaquina = ArrayList<Integer>()
+    var listaColoresUsuario= ArrayList<Integer>()
+    var colorJuego = 0
+    var colorUsuario= 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_botones)
-        var colorJuego = 0
-        var colorUsuario= 0
-        var listaColores = ArrayList<Integer>()
+
+        defineBoton()
+        start()
     }
-    fun start(){
+    fun start() {
         gamePlay()
     }
     fun gamePlay(){
 
-    }
-    fun generarColor(){
+        var i=0
+        while(listaColoresUsuario.equals(listaColoresMaquina)){
+            listaColoresMaquina.add(generarColor())
 
+        }
+
+    }
+    fun defineBoton(){
+        var botonRojo=findViewById<Button>(R.id.botonRojo)
+        var botonVerde=findViewById<Button>(R.id.botonVerde)
+        var botonAmarillo=findViewById<Button>(R.id.botonAmarillo)
+        var botonAzul=findViewById<Button>(R.id.botonAzul)
+        botonRojo.setOnClickListener {
+
+        }
+        botonVerde.setOnClickListener {
+
+        }
+        botonAmarillo.setOnClickListener {
+
+        }
+        botonAzul.setOnClickListener {
+
+        }
+    }
+    fun clickRojo(botonRojo:Button){
+
+    }
+    fun generarColor(): Int {
+        
+        return (0..3).random()
     }
     fun comprobarColor(){
 
     }
+}
+
+private fun <E> ArrayList<E>.add(element: Int) {
+
 }
--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simondice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,9 +13,14 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun llamarJuego(){
-        val boton=findViewById<Button>(R.id.botonInicio)
-        boton.setOnClickListener{
-        
+        val botonInicio=findViewById<Button>(R.id.botonInicio)
+        botonInicio.setOnClickListener{
+        startActivity(
+            Intent(
+                this,
+                Botones::class.java
+            )
+        )
     }
     }
 }
