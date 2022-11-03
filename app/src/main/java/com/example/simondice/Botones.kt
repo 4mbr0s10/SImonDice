337bcee0f8c7178eb040902061162429da741c4b
4mbr0s10 <hperezambrosio@danielcastelao.org>
2022-11-03 14:37:16 +0100

    Cuarto Commit: Backgrounds

--- app/src/main/java/com/example/simondice/Botones.kt
+++ app/src/main/java/com/example/simondice/Botones.kt
@@ -1,18 +1,31 @@
 package com.example.simondice
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.coroutines.delay
+
 
 class Botones : AppCompatActivity() {
-    var listaColoresMaquina = ArrayList<Integer>()
-    var listaColoresUsuario= ArrayList<Integer>()
+//    var listaColoresMaquina = ArrayList<Integer>()
+    var secuenciaMaquina: ArrayList<Int> = arrayListOf()
+//    var listaColoresUsuario= ArrayList<Integer>()
+    var secuenciaUsuario: ArrayList<Int> = arrayListOf()
     var colorJuego = 0
     var colorUsuario= 0
+    lateinit var botonRojo:Button
+    lateinit var botonVerde:Button
+    lateinit var botonAmarillo:Button
+    lateinit var botonAzul:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_botones)
-
+        botonRojo=findViewById<Button>(R.id.botonRojo)
+        botonVerde=findViewById<Button>(R.id.botonVerde)
+        botonAmarillo=findViewById<Button>(R.id.botonAmarillo)
+        botonAzul=findViewById<Button>(R.id.botonAzul)
         defineBoton()
         start()
     }
@@ -22,17 +35,14 @@ class Botones : AppCompatActivity() {
     fun gamePlay(){
 
         var i=0
-        while(listaColoresUsuario.equals(listaColoresMaquina)){
-            listaColoresMaquina.add(generarColor())
-
-        }
+//        while(listaColoresUsuario.equals(listaColoresMaquina)){
+//            listaColoresMaquina.add(generarColor())
+//
+//        }
 
     }
     fun defineBoton(){
-        var botonRojo=findViewById<Button>(R.id.botonRojo)
-        var botonVerde=findViewById<Button>(R.id.botonVerde)
-        var botonAmarillo=findViewById<Button>(R.id.botonAmarillo)
-        var botonAzul=findViewById<Button>(R.id.botonAzul)
+
         botonRojo.setOnClickListener {
 
         }
@@ -49,8 +59,35 @@ class Botones : AppCompatActivity() {
     fun clickRojo(botonRojo:Button){
 
     }
+
+    fun iluminarBoton(){
+        defineBoton()
+        var tiempoIluminado= 0
+        when(generarColor()){
+
+            0 ->{
+                botonRojo.setBackgroundColor(resources.getColor(R.color.rojo_encendido))
+                secuenciaMaquina.add(0)
+
+            }
+            1 -> {
+                botonVerde.setBackgroundColor(resources.getColor(R.color.verde_encendido))
+                secuenciaMaquina.add(1)
+            }
+            2 -> {
+                botonAmarillo.setBackgroundColor(resources.getColor(R.color.amarillo_encendido))
+                secuenciaMaquina.add(2)
+            }
+            3 -> {
+                botonAzul.setBackgroundColor(resources.getColor(R.color.azul_encendido))
+                secuenciaMaquina.add(3)
+            }
+
+
+        }
+    }
     fun generarColor(): Int {
-        
+
         return (0..3).random()
     }
     fun comprobarColor(){
