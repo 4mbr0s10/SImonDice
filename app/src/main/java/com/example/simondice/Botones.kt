4a0b6272cfd76cb3d2a5da110df039d1ef9f0ae9
Héctor Pérez Ambrosio <hperezambrosio@danielcastelao.org>
2022-11-07 18:25:10 +0100

    Commit 4

--- app/src/main/java/com/example/simondice/Botones.kt
+++ app/src/main/java/com/example/simondice/Botones.kt
@@ -4,7 +4,11 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.coroutines.Delay
 import kotlinx.coroutines.delay
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.concurrent.schedule
 
 
 class Botones : AppCompatActivity() {
@@ -35,25 +39,26 @@ class Botones : AppCompatActivity() {
     fun gamePlay(){
 
         var i=0
-//        while(listaColoresUsuario.equals(listaColoresMaquina)){
-//            listaColoresMaquina.add(generarColor())
-//
-//        }
+        while(secuenciaUsuario.equals(secuenciaMaquina)){
+           iluminarBoton()
+            defineBoton()
+
+        }
 
     }
     fun defineBoton(){
 
         botonRojo.setOnClickListener {
-
+            secuenciaUsuario.add(0)
         }
         botonVerde.setOnClickListener {
-
+            secuenciaUsuario.add(1)
         }
         botonAmarillo.setOnClickListener {
-
+            secuenciaUsuario.add(2)
         }
         botonAzul.setOnClickListener {
-
+            secuenciaUsuario.add(3)
         }
     }
     fun clickRojo(botonRojo:Button){
@@ -62,25 +67,37 @@ class Botones : AppCompatActivity() {
 
     fun iluminarBoton(){
         defineBoton()
-        var tiempoIluminado= 0
+        var tiempoIluminado= 2000
         when(generarColor()){
 
             0 ->{
                 botonRojo.setBackgroundColor(resources.getColor(R.color.rojo_encendido))
                 secuenciaMaquina.add(0)
+                Timer().schedule(tiempoIluminado){
+                    botonRojo.setBackgroundColor(resources.getColor(R.color.rojo_apagado))
+                }
 
             }
             1 -> {
                 botonVerde.setBackgroundColor(resources.getColor(R.color.verde_encendido))
                 secuenciaMaquina.add(1)
+                Timer().schedule(tiempoIluminado){
+                    botonVerde.setBackgroundColor(resources.getColor(R.color.verde_apagado))
+                }
             }
             2 -> {
                 botonAmarillo.setBackgroundColor(resources.getColor(R.color.amarillo_encendido))
                 secuenciaMaquina.add(2)
+                Timer().schedule(tiempoIluminado){
+                    botonAmarillo.setBackgroundColor(resources.getColor(R.color.amarillo_apagado))
+                }
             }
             3 -> {
                 botonAzul.setBackgroundColor(resources.getColor(R.color.azul_encendido))
                 secuenciaMaquina.add(3)
+                Timer().schedule(tiempoIluminado){
+                    botonAzul.setBackgroundColor(resources.getColor(R.color.azul_apagado))
+                }
             }
 
 
@@ -93,6 +110,10 @@ class Botones : AppCompatActivity() {
     fun comprobarColor(){
 
     }
+}
+
+private fun Timer.schedule(tiempoIluminado: Int, function: () -> Unit) {
+
 }
 
 private fun <E> ArrayList<E>.add(element: Int) {
